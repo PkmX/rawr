669a66819b92a7066f91f744f3a8372e614d6189
PkmX <pkmx.tw@gmail.com>
2016-09-09 15:59:45 +0800

    Update to ghc-datasize == 0.2.*

--- tests/datasize/Main.hs
+++ tests/datasize/Main.hs
@@ -16,23 +16,22 @@ data Id a = Id a deriving (Generic, NFData)
 main :: IO ()
 main = defaultMain $ testGroup "ghc-datasize"
     [ testCase "R0" $ do
-        s <- rsize $!! ()
-        s' <- rsize $!! R ()
+        s <- recursiveSize $!! ()
+        s' <- recursiveSize $!! R ()
         s @?= s'
     , testCase "R1" $ do
-        s <- rsize $!! Id True
-        s' <- rsize $!! R ( #a := True )
+        s <- recursiveSize $!! Id True
+        s' <- recursiveSize $!! R ( #a := True )
         s @?= s'
     , testCase "R2" $ do
         let a = 1 :: Int
-        s <- rsize $!! (a, True)
-        s' <- rsize $!! R ( #a := a, #b := True )
+        s <- recursiveSize $!! (a, True)
+        s' <- recursiveSize $!! R ( #a := a, #b := True )
         s @?= s'
     , testCase "R3" $ do
         let a = 1 :: Int
             c = "Hello world"
-        s <- rsize $!! (a, True, c)
-        s' <- rsize $!! R ( #a := a, #b := True, #c := c )
+        s <- recursiveSize $!! (a, True, c)
+        s' <- recursiveSize $!! R ( #a := a, #b := True, #c := c )
         s @?= s'
     ]
-  where rsize = recursiveSize :: (a -> IO Integer)
